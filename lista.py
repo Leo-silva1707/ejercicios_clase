2387d4662155c7b02f0cf7701e407bd2321fec45
Leo-silva1707 <rodriguez.silva.leonardo@iescamas.es>
2024-12-17 10:38:06 +0100

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,8 @@
+lst = [5, 4, 3, 11, 32, 33]
+acumulador = 0
+for br in range (0, len(lst)):
+    print(lst[br])
+    acumulador = acumulador + lst[br]
+    print("el resukltadi es:", acumulador)
+
+
